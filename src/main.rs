d9244f85c9fe8717d9b8fe9e043a60663366a803
Piper Inns Hall <piper.innshall@gmail.com>
2026-04-04 22:13:19 +1300

    chore: Complete core 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use anyhow::{Context, Error, Result};
+use anyhow::{Context, Result};
 use opencv::core::{
     Mat, MatTraitConst, MatTraitConstManual, MatTraitManual, Point2i, Scalar, Vec3b, Vector,
 };
+use rand::prelude::RngExt;
 
 fn image_read(path: &str) -> Result<Mat> {
     Ok(opencv::imgcodecs::imread(
@@ -27,27 +28,29 @@ fn main() -> Result<()> {
 fn core(params: &Vector<i32>) -> Result<()> {
     let im_src = image_read("Source.jpg")?;
     let im_target = image_read("Target.jpg")?;
+
+    // Initialization
     let nnf = initialize_nnf(&im_src, &im_target).context("Initialize NNF")?;
-    let nnf = improve_nnf(&nnf, &im_src).context("Improve NNF")?;
-    let im_nnf = nnf_to_image(&nnf, &im_target).context("NNF to image")?;
-    Ok(image_write("Core.jpg", &im_nnf, &params)?)
+    let core1 = nnf_to_image(&nnf, &im_src).context("NNF to image")?;
+    Ok(image_write("Core1.jpg", &core1, &params)?)
+
+    // let nnf = improve_nnf(&nnf, &im_src).context("Improve NNF")?;
 }
 
 fn initialize_nnf(src: &Mat, target: &Mat) -> Result<Mat> {
+    let mut rng = rand::rng();
     let mut nnf = Mat::new_rows_cols_with_default(
         target.rows(),
         target.cols(),
         opencv::core::CV_32SC2,
         Scalar::default(),
     )?;
-    nnf.data_typed_mut::<Point2i>()?
-        .iter_mut()
-        .enumerate()
-        .try_for_each(|(idx, out)| {
-            let x = idx as i32 % target.cols();
-            let y = idx as i32 / target.cols();
-            Ok::<(), Error>(())
-        });
+    nnf.data_typed_mut::<Point2i>()?.iter_mut().for_each(|out| {
+        *out = Point2i::new(
+            rng.random_range(0..src.cols()),
+            rng.random_range(0..src.rows()),
+        )
+    });
     Ok(nnf)
 }
 
@@ -67,15 +70,14 @@ fn nnf_to_image(nnf: &Mat, src: &Mat) -> Result<Mat> {
         .zip(dst.data_typed_mut::<Vec3b>()?.iter_mut())
         .try_for_each(|(p, out)| {
             anyhow::ensure!(
-                p.x >= 0 && p.y >= 0 && p.x < src.rows() && p.y < src.cols(),
+                p.x >= 0 && p.y >= 0 && p.x < src.cols() && p.y < src.rows(),
                 "Coordinate {:?} is outside of source.",
                 p
             );
-            let r = (p.y * 255 / src.cols()) as u8;
-            let g = (p.x * 255 / src.rows()) as u8;
+            let r = (p.x * 255 / src.cols()) as u8;
+            let g = (p.y * 255 / src.rows()) as u8;
             let b = 255 - u8::max(r, g);
-            *out = Vec3b::from([b, g, r]);
-            Ok(())
+            Ok(*out = Vec3b::from([b, g, r]))
         })?;
     Ok(dst)
 }
