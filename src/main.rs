3906006392de7702479e1b915a4e809eeaa692c7
Piper Inns Hall <piper.innshall@gmail.com>
2026-04-15 17:29:23 +1200

    chore: Core 3 bounds working

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use anyhow::{Context, Result};
 use opencv::{
     boxed_ref::BoxedRef,
     core::{
-        Mat, MatTraitConst, MatTraitConstManual, MatTraitManual, Point2i, Rect, Scalar, Vec3b,
+        Mat, MatTrait, MatTraitConst, MatTraitConstManual, MatTraitManual, Point2i, Rect, Scalar, Vec3b,
         Vector,
     },
 };
@@ -31,6 +31,8 @@ fn core(params: &Vector<i32>) -> Result<()> {
     let rand_nnf = rand_nnf(&nnf, &src_border, &target_border, &mut d, patch).context("Rand")?;
     image::write("Core2.jpg", &image::from_nnf(&rand_nnf, &im_src)?, &params)?;
 
+    let prop_nnf = propagate_nnf(&rand_nnf, &src_border, &target_border, &mut d, patch).context("Prop")?;
+    image::write("Core3.jpg", &image::from_nnf(&prop_nnf, &im_src)?, &params)?;
     Ok(())
 }
 
@@ -53,7 +55,6 @@ fn distance_over_cost(
         })
         .collect()
 }
-
 
 fn initialize_nnf(src: &Mat, target: &Mat) -> Result<Mat> {
     let mut rng = rand::rng();
@@ -92,22 +93,25 @@ fn rand_nnf(
         .zip(nnf.data_typed::<Point2i>()?.iter())
         .enumerate()
         .try_for_each(|(idx, (out, p))| -> Result<()> {
-            let mut best_offset = *p;
             let px = idx as i32 % nnf.cols();
             let py = idx as i32 / nnf.cols();
+
+            let mut best_offset = *p;
             for i in 0..5 {
                 let search_radius = max_dimension * (1f32 / 2f32).powi(i);
                 let ux = rand_propose(p.x, src_border.cols() - patch, search_radius, &mut rng);
                 let uy = rand_propose(p.y, src_border.rows() - patch, search_radius, &mut rng);
                 let improved = improved_nnf(
                     &[(
-                        Mat::roi(src_border, Rect::new(ux, uy, patch, patch))?,
-                        Mat::roi(target_border, Rect::new(px, py, patch, patch))?,
+                        Mat::roi(src_border, Rect::new(ux, uy, patch, patch))?, 
+                        Mat::roi(target_border, Rect::new(px, py, patch, patch))?, 
+                        ux,
+                        uy,
                     )],
                     d[idx],
                 );
-                if let Some(ssd) = improved {
-                    best_offset = Point2i::new(ux, uy);
+                if let Some((ssd, x, y)) = improved {
+                    best_offset = Point2i::new(x, y);
                     d[idx] = ssd;
                 }
             }
@@ -121,13 +125,98 @@ fn rand_propose(position: i32, max: i32, radius: f32, rng: &mut impl rand::prelu
     (position as f32 + rng.random_range(-1f32..=1f32) * radius).clamp(0.0, max as f32) as i32
 }
 
+fn propagate_nnf(
+    nnf: &Mat,
+    src_border: &Mat,
+    target_border: &Mat,
+    d: &mut Vec<f32>,
+    patch: i32,
+) -> Result<Mat> {
+    let mut dst = Mat::new_rows_cols_with_default(
+        nnf.rows(),
+        nnf.cols(),
+        opencv::core::CV_32SC2,
+        Scalar::default(),
+    )?;
+
+    for py in 1..nnf.rows() - 1 {
+        for px in 1..nnf.cols() - 1 {
+            let idx = py * nnf.cols() + px;
+
+            let left_x = px - 1;
+            let up_y = py - 1;
+
+            let mut best_offset = *nnf.at_2d::<Point2i>(py, px)?;
+            let improved = improved_nnf(
+                &[
+                    (
+                        Mat::roi(src_border, Rect::new(left_x, py, patch, patch))?,
+                        Mat::roi(target_border, Rect::new(px, py, patch, patch))?,
+                        left_x,
+                        py,
+                    ),
+                    (
+                        Mat::roi(src_border, Rect::new(px, up_y, patch, patch))?,
+                        Mat::roi(target_border, Rect::new(px, py, patch, patch))?,
+                        px,
+                        up_y,
+                    ),
+                ],
+                d[idx as usize],
+            );
+            if let Some((ssd, x, y)) = improved {
+                best_offset = *nnf.at_2d::<Point2i>(y, x)?;
+                d[idx as usize] = ssd;
+            }
+            *dst.at_2d_mut::<Point2i>(py, px)? = best_offset;
+        }
+    }
+
+    // dst.data_typed_mut::<Point2i>()?
+    //     .iter_mut()
+    //     .enumerate()
+    //     .try_for_each(|(idx, out)| -> Result<()> {
+    //         let px = 1 + (idx as i32 % nnf.cols());
+    //         let py = 1 + (idx as i32 / nnf.cols());
+    //         let left_x = px;
+    //         let up_y = py;
+    //         let mut best_offset = *nnf.at_2d::<Point2i>(py, px)?;
+    //         let improved = improved_nnf(
+    //             &[
+    //                 (
+    //                     Mat::roi(src_border, Rect::new(left_x, py, patch, patch))?,
+    //                     Mat::roi(target_border, Rect::new(px, py, patch, patch))?,
+    //                     left_x,
+    //                     py,
+    //                 ),
+    //                 (
+    //                     Mat::roi(src_border, Rect::new(px, up_y, patch, patch))?,
+    //                     Mat::roi(target_border, Rect::new(px, py, patch, patch))?,
+    //                     px,
+    //                     up_y,
+    //                 ),
+    //             ],
+    //             d[idx],
+    //         );
+    //         if let Some((ssd, x, y)) = improved {
+    //             best_offset = *nnf.at_2d::<Point2i>(y, x)?;
+    //             d[idx] = ssd;
+    //         }
+    //         *out = best_offset;
+    //         Ok(())
+    //     })?;
+
+    Ok(dst)
+}
+
 fn improved_nnf(
-    roi_pair: &[(BoxedRef<'_, Mat>, BoxedRef<'_, Mat>)],
+    roi_pair: &[(BoxedRef<'_, Mat>, BoxedRef<'_, Mat>, i32, i32)],
     current_ssd: f32,
-) -> Option<f32> {
-    roi_pair.iter().fold(None, |_, (proposed, patch)| {
-        let new_ssd = sum_squared_differences(&proposed.try_clone().ok()?, &patch.try_clone().ok()?).ok()?;
-        (new_ssd < current_ssd).then_some(new_ssd)
+) -> Option<(f32, i32, i32)> {
+    roi_pair.iter().fold(None, |_, (proposed, patch, x, y)| {
+        let new_ssd =
+            sum_squared_differences(&proposed.try_clone().ok()?, &patch.try_clone().ok()?).ok()?;
+        (new_ssd < current_ssd).then_some((new_ssd, *x, *y))
     })
 }
 
